959bd9fc4145d951ec3cf913e119f4358b792e29
tuna2134 <hello@tuna2134.dev>
2025-10-25 10:24:32 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-mkdir -p ip-filters
+IP_FILTER=""
 
 IFS=$'\n'; for item in $(cat config.json | jq -c '.[]'); do
     ASN=$(echo $item | jq .asn)
     IPV4=$(echo $item | jq .ipv4)
     IPV6=$(echo $item | jq .ipv6)
-    IP_FILTER=""
     if [ $IPV4 = "true" ]; then
         IP_FILTER+=$(bgpq4 -b AS${ASN} -l "define as${ASN}_ips")
         IP_FILTER+=$'\n'
@@ -13,12 +12,14 @@ IFS=$'\n'; for item in $(cat config.json | jq -c '.[]'); do
     fi
     if [ $IPV6 = "true" ]; then
         IP_FILTER+=$(bgpq4 -6 -b AS${ASN} -l "define as${ASN}_ipv6s")
+        IP_FILTER+=$'\n'
     fi
-    if [ -f ip-filters/AS${ASN}_ip.conf ]; then
-        if [ "$(cat ip-filters/AS${ASN}_ip.conf)" != "$IP_FILTER" ]; then
-            echo "Apply to BIRD3..."
-            birdc configure
-        fi
-    fi
-    echo "$IP_FILTER" > ip-filters/AS${ASN}_ip.conf
 done
+
+if [ -f ip-filters/AS${ASN}_ip.conf ]; then
+    if [ "$(cat ip-filters/AS${ASN}_ip.conf)" != "$IP_FILTER" ]; then
+        echo "Apply to BIRD3..."
+        birdc configure
+    fi
+fi
+echo "$IP_FILTER" > ip-filter.conf
